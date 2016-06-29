f935094a207e06c6ebf21ca0088aa6de59929b0e
Anthony DENAUD <git@anthonydenaud.com>
2016-06-29 20:31:13 +0100

    fix

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -21,8 +21,9 @@ class NotificationController < ApplicationController
 
   def auth_callback
     auth = request.env['omniauth.auth']
-    session[:current_user] = { :nickname => auth.info['nickname'],
-                               :image => auth.info['image'],
+
+    session[:current_user] = { :nickname => auth.info.nickname,
+                               :image => auth.info.image,
                                :uid => auth.uid }
 
     redirect_to root_url
