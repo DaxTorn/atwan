697b317323f7ad3bc207c01e31596d32768400e7
Anthony DENAUD <git@anthonydenaud.com>
2016-07-01 13:45:52 +0100

    Login + register

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,10 +9,51 @@ class UserController < ApplicationController
 
   def auth_callback
     auth = request.env['omniauth.auth']
-
     session[:current_user] = { :nickname => auth.info.nickname,
                                :image => auth.info.image,
                                :uid => auth.uid }
+
+    redirect_to root_url
+  end
+
+  def new
+  end
+
+  def create
+
+    md5 = Digest::MD5.new
+
+    username = params[:username]
+    email = params[:email]
+    steamid = params[:steamid]
+    password = params[:password]
+    cpassword = params[:cpassword]
+
+    user = User.new
+    user.nickname = username
+    user.email = email
+    user.password = md5 << password
+    user.steamid = steamid
+    user.save
+
+    redirect_to root_url
+  end
+
+
+  def login
+
+    md5 = Digest::MD5.new
+
+    username =  params[:username]
+    password = (md5 << params[:password]).to_s
+
+    user = User.find_by(nickname: username, password: password)
+
+    unless user.nil?
+      session[:current_user] = { :nickname => username,
+                                 :uid => user.steamid }
+    end
+
     redirect_to root_url
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'notification#index'
-  get 'logout' => 'user#logout'
+  post 'sign-in' => 'user#login'
+  get 'sign-out' => 'user#logout'
+  get 'sign-up' => 'user#new'
+  post 'sign-up' => 'user#create'
   post 'notification' => 'notification#create'
   post 'auth/steam/callback' => 'user#auth_callback'
 end
