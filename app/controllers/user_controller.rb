3b50f2e8c8a7699593b8afbd9cd27bbdcb899710
Anthony DENAUD <git@anthonydenaud.com>
2016-07-04 17:18:19 +0100

    register validation

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,60 +1,56 @@
 class UserController < ApplicationController
 
-  skip_before_filter :verify_authenticity_token
+    skip_before_filter :verify_authenticity_token
 
-  def logout
-    session.clear
-    redirect_to root_url
-  end
-
-  def auth_callback
-    auth = request.env['omniauth.auth']
-    session[:current_user] = { :nickname => auth.info.nickname,
-                               :image => auth.info.image,
-                               :uid => auth.uid }
-
-    redirect_to root_url
-  end
-
-  def new
-  end
-
-  def create
-
-    md5 = Digest::MD5.new
-
-    username = params[:username]
-    email = params[:email]
-    steamid = params[:steamid]
-    password = params[:password]
-    cpassword = params[:cpassword]
-
-    user = User.new
-    user.nickname = username
-    user.email = email
-    user.password = md5 << password
-    user.steamid = steamid
-    user.save
-
-    redirect_to root_url
-  end
-
-
-  def login
-
-    md5 = Digest::MD5.new
-
-    username =  params[:username]
-    password = (md5 << params[:password]).to_s
-
-    user = User.find_by(nickname: username, password: password)
-
-    unless user.nil?
-      session[:current_user] = { :nickname => username,
-                                 :uid => user.steamid }
+    def logout
+        session.clear
+        redirect_to root_url
     end
 
-    redirect_to root_url
-  end
+    def auth_callback
+        auth = request.env['omniauth.auth']
+        session[:current_user] = {:nickname => auth.info.nickname,
+                                  :image => auth.info.image,
+                                  :uid => auth.uid}
+
+        redirect_to root_url
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_url
+        else
+            render('user/new')
+        end
+    end
+
+
+    def login
+
+        md5 = Digest::MD5.new
+
+        username = params[:username]
+        password = (md5 << params[:password]).to_s
+
+        user = User.find_by(nickname: username, password: password)
+
+        unless user.nil?
+            session[:current_user] = {:nickname => username,
+                                      :uid => user.steamid}
+        end
+
+        redirect_to root_url
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:nickname, :steamid, :email, :password, :password_confirmation)
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ActiveRecord::Base
-  has_and_belongs_to_many :servers
+    has_and_belongs_to_many :servers
+
+    before_save :encrypt_password
+    after_save :clear_password
+
+    validates :nickname, :email, :steamid, :password, presence: true
+    validates :steamid, length: {is: 16}
+    validates :password, confirmation: true
+
+    def encrypt_password
+        md5 = Digest::MD5.new
+        self.password = md5 << password
+    end
+
+    def clear_password
+        self.password = nil
+    end
+
 end
